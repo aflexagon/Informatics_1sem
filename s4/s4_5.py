9a9238c29b6a54e493e1399079428989c2a5a2c7
aflexagon <core.ui>
2023-10-06 17:00:28 +0300

    06.10

--- s4/s4_5.py
+++ s4/s4_5.py
@@ -3,16 +3,16 @@ import numpy as np
 import pandas as pd
 
 
-tab = pd.read_csv('BTC_data.csv')
+tmp = pd.read_csv('BTC_data.csv')
 
-y = list(map(float, list(tab["close"])))
-x = list(tab['time'])
-for i in range(len(((tab)))):
+y = list(map(float, list(tmp["close"])))
+x = list(tmp['time'])
+for i in range(len(((tmp)))):
     x[i] = f'{x[i][8:10]} - {x[i][5:7]} - {x[i][:4]}'
-    print(x[i])
+
 fig = plt.figure()
 ax1 = plt.subplot(111)
-xtab = list(range(len(y)))
+xtmp = list(range(len(y)))
 ax1.plot(x, y, color = 'b')
 
 ax1.grid()
